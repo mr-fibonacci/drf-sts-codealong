4bc1cdac89270730fad95325fdc63fa1db15b8b0
mr-fibonacci <adamlapinski1@gmail.com>
2021-05-21 10:04:48 +0000

    006: - added login/ logout views to browsable API - wrote the IsOwnerOrReadOnly permission - added an extra is_owner field to ProfileSerializer - passed down the request via context to populate the is_owner field

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,11 +3,21 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    """ Profile serializer, owner field not editable. """
+    """
+    Profile serializer, owner field not editable.
+    get_is_owner needs access to request via context to determine
+    if a given user owns the profile (is_owner).
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image'
+            'id', 'owner', 'is_owner', 'created_at',
+            'updated_at', 'name', 'content', 'image'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,15 +2,22 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(APIView):
-    """ View to list all profiles """
+    """
+    View to list all profiles.
+    Serializer needs access to the request via context to determine
+    if the user making the request owns the profile.
+    """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
 
@@ -19,24 +26,32 @@ class ProfileDetail(APIView):
     The get_object method handles the 404 profile doesn't exist case.
     get: retrieve a profile.
     put: update a profile.
+    Thanks to the IsOwnerOrReadOnly permission,
+    only the profile owner can make changes to it.
+    Serializer needs access to the request via context to determine
+    if the user making the request owns the profile.
     """
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
